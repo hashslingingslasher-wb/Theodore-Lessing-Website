be40695037e19c560a951441b0957f10f0db405f
Will Barlow <58278392+hashslingingslasher-wb@users.noreply.github.com>
2025-04-17 10:30:41 -0600

    Initial 2

---
+++ Program.cs
@@ -0,0 +1,64 @@
+using System.Configuration;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using MailKit;
+using Services;
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        CreateHostBuilder(args).Build().Run();
+    }
+    public static IHostBuilder CreateHostBuilder(string[] args) =>
+    Host.CreateDefaultBuilder(args).ConfigureWebHostDefaults(weBuilder =>
+    {
+        weBuilder.ConfigureAppConfiguration((context,config) => 
+        {
+            config.AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
+            config.AddEnvironmentVariables();          
+        })
+        .UseStartup<Startup>();
+    });
+}
+public class Startup
+{
+    public Startup(IConfiguration configuration)
+    {
+        Configuration = configuration;
+    }
+    public IConfiguration Configuration {get; }
+    public void ConfigureServices(IServiceCollection services)
+    {
+        services.Configure<SmtpSettings>(Configuration.GetSection("Smtp"));
+        services.AddSingleton<IEmailService, EmailService>();
+        services.AddControllersWithViews();
+        services.AddLogging();
+       
+    }
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+    {
+        if(env.IsDevelopment())
+        {
+            app.UseDeveloperExceptionPage();
+        }
+        else
+        {
+            app.UseExceptionHandler("/Home/Error");
+            app.UseHsts();
+        }
+        app.UseHttpsRedirection();
+        app.UseStaticFiles();
+        app.UseRouting();
+        app.UseAuthorization();
+        app.UseEndpoints(endpoints =>
+        {
+            endpoints.MapControllerRoute(
+                name: "default",
+                pattern: "{controller=Home}/{action=Index}/{id?}"
+            );
+        });
+    }
+}
